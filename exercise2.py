9a75fe629d99a92d15b361b580e1d4d7b43ef055
Lara Mol <89733492+LaraMol@users.noreply.github.com>
2021-09-24 11:40:34 +0200

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,36 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 2')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.grab()
+for x in range(9):
+    robotArm.moveRight()
+
+
+robotArm.drop()
+for i in range(5):
+    robotArm.moveLeft()
+
+robotArm.grab()
+for l in range (5):
+    robotArm.moveRight()
+robotArm.drop()
+
+for m in range (2):
+    robotArm.moveLeft()
+
+robotArm.grab()
+
+for m in range (2):
+    robotArm.moveRight()
+
+
+
+robotArm.drop()
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait() 
